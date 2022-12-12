934fdcdd9e0feb25b36d1613349f2c9c3d232a1d
TheGatesScratch <mtkarels@outlook.com>
2022-12-12 21:08:46 +0100

    messing around

--- src/test_game/client.ts
+++ src/test_game/client.ts
@@ -1,14 +1,14 @@
-import { Application } from "pixi.js";
-import { ComponentType, GatesECS } from "../LIB/GatesECS";
+import { Container, Graphics } from "pixi.js";
+import { ComponentType } from "../LIB/GatesECS";
+import { ComponentTypes, Scene } from "../LIB/GatesEngine";
+
+const PositionComponent = ComponentTypes.add(new ComponentType<{ x: number, y: number }>("position"));
+const RenderContainerComponent = ComponentTypes.add(new ComponentType<{ container: Container, positionComp: number }>("render_container"));
+
+const mainSene = new Scene();
 
 
-const APP = new Application({
-    resizeTo: window,
-    autoDensity: true,
-});
 
-const ECS = new GatesECS();
-const PositionComponent = new ComponentType<{x:number, y:number}>("position");
-
-const entity1 = ECS.entity();
-ECS.addComponent(entity1, PositionComponent.create({x:3,y:4}));
+const player = mainSene.entity();
+const playerPos = mainSene.addComponent(player, PositionComponent.create({ x: 20, y: 50 }));
+mainSene.addComponent(player, RenderContainerComponent.create({ positionComp: playerPos, container: new Graphics().beginFill(0xfff).drawRect(0, 0, 40, 30) }));
