d97d72b90d2af614b31bbd9231f477d331f74e89
TheGatesScratch <mtkarels@outlook.com>
2022-12-13 19:44:51 +0100

    Small changes

--- src/test_game/client.ts
+++ src/test_game/client.ts
@@ -1,3 +1,5 @@
+// @ts-strict
+
 import { Container, Graphics } from "pixi.js";
 import { ComponentType } from "../LIB/GatesECS";
 import { ComponentTypes, Scene } from "../LIB/GatesEngine";
@@ -6,8 +8,6 @@ const PositionComponent = ComponentTypes.add(new ComponentType<{ x: number, y: n
 const RenderContainerComponent = ComponentTypes.add(new ComponentType<{ container: Container, positionComp: number }>("render_container"));
 
 const mainSene = new Scene();
-
-
 
 const player = mainSene.entity();
 const playerPos = mainSene.addComponent(player, PositionComponent.create({ x: 20, y: 50 }));
